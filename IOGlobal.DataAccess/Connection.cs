c3eff773654c4e87ae1e6409009a7ed288417d70
ValeryChicas <cc0493022024@unab.edu.sv>
2025-04-04 19:46:47 -0600

    Cambie la capa donde estan los formularios porque no estaba en .net y por eso no aparecie el app.config (upss)

---
+++ IOGlobal.DataAccess/Connection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOGlobal.DataAccess
+{
+    public class Connection
+    {
+        //cadena de coneccion para la base de datos
+                                                            //Server name                 //Nombre de la bd a la que se quiere conectar
+        protected string _connectionString = "Data Source = LAPTOP-DM50CU2S\\SQLEXPRESS ; Initial Catalog= Animalandia01 ; Integrated Security = true";
+    }
+}
